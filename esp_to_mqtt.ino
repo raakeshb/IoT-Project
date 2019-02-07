6ca63f0699e5ecca7fc7e03c69f0f6005879d000
raakeshb <raakesh.balaji@gmail.com>
2019-02-07 18:48:21 +0530

    Add files via upload

---
+++ esp_to_mqtt.ino
@@ -0,0 +1,83 @@
+//***** Raakesh*****======> LDR and  ULTRASONIC
+//https://www.arduino.cc/reference/en/#functions ====== for all fn
+
+//https://www.arduino.cc/reference/en/language/functions/advanced-io/pulsein -FOR PULSE IN FUNCTION USED BELOW
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <WiFiClient.h>
+
+const char* ssid = "Balaji";
+const char* password =  "barrr9940201157";
+const char* mqttServer = "192.168.0.5" ;
+const int mqttPort = 1883;
+const char* mqttUser = "raakesh";
+const char* mqttPassword = "helloworld";
+WiFiClient espClient;
+PubSubClient client(espClient);
+int outputpin= A0;
+
+
+void setup() {
+  Serial.begin(115200);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting to WiFi..");
+  }
+  Serial.println("Connected to the WiFi network");
+
+  client.setServer(mqttServer, mqttPort);
+  client.setCallback(callback);
+
+  while (!client.connected()) {
+    Serial.println("Connecting to MQTT...");
+
+    if (client.connect("ESP8266Client", mqttUser, mqttPassword )) {
+
+      Serial.println("connected");
+
+    } else {
+
+      Serial.print("failed with state ");
+      Serial.print(client.state());
+      delay(2000);
+
+    }
+  }
+
+}
+
+
+void callback(char* topic, byte* payload, unsigned int length) {
+ 
+
+  Serial.print("Message arrived in topic: ");
+  Serial.println(topic);
+
+  Serial.print("Message:");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+
+  Serial.println();
+  Serial.println("-----------------------");
+
+}
+
+
+void loop() {
+  int analogValue = analogRead(outputpin);
+float millivolts = (analogValue/1024.0) * 3300; //3300 is the voltage provided by NodeMCU
+float celsius = millivolts/10;
+Serial.print("in DegreeC=   ");
+Serial.println(celsius);
+float fahrenheit = ((celsius * 9)/5 + 32);
+Serial.print(" in Farenheit=   ");
+Serial.println(fahrenheit);
+delay(1000);
+  client.publish("esp/temp", String(celsius).c_str());
+  client.publish("esp/temp", String(fahrenheit).c_str());
+  client.loop();
+}
---
+++ mqtttokafka.py
@@ -0,0 +1,32 @@
+import paho.mqtt.client as mqtt
+from kafka import KafkaProducer
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("esp/temp")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+    producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
+    
+    future = producer.send('esp/temp', msg.payload)
+    
+
+client = mqtt.Client()
+
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect("192.168.0.5", 1883, 60)
+client.username_pw_set(username="raakesh",password="raakesh30")
+
+
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+client.loop_forever()
