6219f483f1b021978fae154cf1645850cd4df80f
raakeshb <raakesh.balaji@gmail.com>
2019-03-08 21:53:37 +0530

    Update mqtttokafka.py

--- mqtttokafka.py
+++ mqtttokafka.py
@@ -7,22 +7,22 @@ def on_connect(client, userdata, flags, rc):
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("testok")
+    client.subscribe("MQTT TOPIC")
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
     producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
     
-    future = producer.send('yui', msg.payload)
+    future = producer.send('TOPIC CREATED IN KAFKA', msg.payload)
     
 
 client = mqtt.Client()
 
 client.on_connect = on_connect
 client.on_message = on_message
-client.connect("192.168.0.5", 1883, 60)
-client.username_pw_set(username="raakesh",password="helloworld")
+client.connect("IP_ADDRESS_OF_MQTT", 1883, 60)
+client.username_pw_set(username="MQTT_USER_HERE",password="MQTT_PASSWORD HERE")
 
 
 # Blocking call that processes network traffic, dispatches callbacks and
